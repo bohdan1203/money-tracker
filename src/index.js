aa291038bf2c0de9cfae5e5a292a3d7a044fab3a
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 16:08:55 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router";
+import App from "./App";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route path="/money-tracker" exact component={App} />
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-

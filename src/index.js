5927540fc02e7cac422f99fecbb818f68f3bb98c
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 18:42:57 +0300

    ddsdfs

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import { Switch, Route } from "react-router";
 import App from "./App";
 
 
+function Asdf() {
+  return 'ASDFGHJKL'
+}
+
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <Router>
-  //     <Switch>
-  //       <Route path="/"  component={App} />
-  //     </Switch>
-  //   </Router>
-  // </React.StrictMode>
-  <App />,
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/"  component={App} />
+        <Route  path="/asdf"  component={Asdf} />
+      </Switch>
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );

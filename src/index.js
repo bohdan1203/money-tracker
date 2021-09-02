99253e1e56b2a426e133b103a0280f28046fe150
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 17:05:34 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router";
 import App from "./App";
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route path="/money-tracker" exact component={App} />
+        <Route exact path="/money-project"  component={App} />
       </Switch>
     </Router>
   </React.StrictMode>,
